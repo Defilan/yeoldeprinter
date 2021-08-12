572375d7d0cb876fcd59f17f33bc3c3af066be43
Christopher P. Maher <chris@mahercode.io>
2021-08-12 14:40:02 -0700

    Adding Keyboard module and testing on Linux machine

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from utils import getfilecontentslist, getrandomjokefromlist
+from utils import getfilecontentslist, getrandomjokefromlist, keyboardwait
 
-print("\n")
-print("Time for a funny one-liner!\n")
-jokeList = getfilecontentslist("oneliners")
-print(getrandomjokefromlist(jokeList))
-print("Thanks for using Ye Olde Printer!")
-print("\n")
+def printstuffy():
+    print(keyboardwait)
+
+printstuffy()
+# print("\n")
+# print("Time for a funny one-liner!\n")
+# jokeList = getfilecontentslist("oneliners")
+# print(getrandomjokefromlist(jokeList))
+# print("Thanks for using Ye Olde Printer!")
+# print("\n")
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from random import randint
+import keyboard
 
 
 # Get the jokes out of a .txt file and into a list
@@ -8,6 +9,12 @@ def getfilecontentslist(filename):
     ourfile.close()
     return contentlist
 
+
 # Choose a random joke from a given joke list
 def getrandomjokefromlist(jokelist):
     return jokelist[randint(0, len(jokelist) - 1)]
+
+
+def keyboardwait():
+    keyboard.wait('esc')
+    return "You waited for esc, good job"
