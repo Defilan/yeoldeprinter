e03724d1f74763fda20c20971d52903e6b7390c7
Christopher P. Maher <chris@mahercode.io>
2021-08-11 17:34:04 -0700

    Refactoring some of the code to meet SonarQube recommendations
    Signed-off-by: Christopher P. Maher <chris@mahercode.io>

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-import os
-import subprocess
-from utils import getFileContentsList, getRandomJokeFromList
-
-
-# def quotemaker():
-#     return "This is a funny joke"
-#
-#
-# file = open("/tmp/example.txt", "w")
-# file.write("This is funny")
-# print(file.read("/tmp/example.txt"))
-# file.close()
-
-# sender = "This is a test of the program"
-
-
-# lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
-# lpr.stdin.write(b"Printing from Python!")
+from utils import getfilecontentslist, getrandomjokefromlist
 
 print("\n")
 print("Time for a funny one-liner!\n")
-jokeList = getFileContentsList("oneliners")
-print(getRandomJokeFromList(jokeList))
+jokeList = getfilecontentslist("oneliners")
+print(getrandomjokefromlist(jokeList))
 print("Thanks for using Ye Olde Printer!")
 print("\n")
--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 from random import randint
 
-# Get the jokes out of a .txt file and into a list
-def getFileContentsList(filename):
-    file = open("resources/" + filename + ".txt", "r")
-    contentList = file.readlines()
-    file.close()
-    return contentList
 
-def getRandomJokeFromList(jokeList):
-    return jokeList[randint(0, len(jokeList))]
+# Get the jokes out of a .txt file and into a list
+def getfilecontentslist(filename):
+    ourfile = open("resources/" + filename + ".txt", "r")
+    contentlist = ourfile.readlines()
+    ourfile.close()
+    return contentlist
+
+
+def getrandomjokefromlist(jokelist):
+    return jokelist[randint(0, len(jokelist))]
