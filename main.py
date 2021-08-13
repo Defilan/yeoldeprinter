8cce3f4e73435a687280111285355a833e334c1c
nanetteranes <nanette.ranes@alaskaair.com>
2021-08-13 09:03:15 -0700

    Style updates according to https://pep8.org/

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from utils import getfilecontentslist, getrandomjokefromlist
+from utils import get_file_contents, get_random_joke
 
+# Currently the main process, prints a joke to the terminal :)
 print("\n")
 print("Time for a funny one-liner!\n")
-jokeList = getfilecontentslist("oneliners")
-print(getrandomjokefromlist(jokeList))
+joke_list = get_file_contents("oneliners")
+print(get_random_joke(joke_list))
 print("Thanks for using Ye Olde Printer!")
 print("\n")
--- utils.py
+++ utils.py
@@ -1,13 +1,12 @@
 from random import randint
 
-
 # Get the jokes out of a .txt file and into a list
-def getfilecontentslist(filename):
-    ourfile = open("resources/" + filename + ".txt", "r")
-    contentlist = ourfile.readlines()
-    ourfile.close()
-    return contentlist
+def get_file_contents(filename):
+    our_file = open("resources/" + filename + ".txt", "r")
+    content_list = our_file.readlines()
+    our_file.close()
+    return content_list
 
 # Choose a random joke from a given joke list
-def getrandomjokefromlist(jokelist):
-    return jokelist[randint(0, len(jokelist) - 1)]
+def get_random_joke(joke_list):
+    return joke_list[randint(0, len(joke_list) - 1)]
